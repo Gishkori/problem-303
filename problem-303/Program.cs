7417fd81535e60caea00275f4c0742d84d845b47
Ali Asghar Gishkori <gishkori@hotmail.com>
2024-09-11 22:13:48 +1000

    SumRange finder added

---
+++ problem-303/Program.cs
@@ -0,0 +1,32 @@
+﻿
+public class NumArray
+{
+
+    private int[] numArray;
+
+    public NumArray(int[] nums)
+    {
+        numArray = nums;
+    }
+
+    public int SumRange(int left, int right)
+    {
+        int sum = 0;
+        for (int i = left; i <= right && i < numArray.Length; i++)
+        {
+            sum = sum + numArray[i];
+        }
+        return sum;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        NumArray obj = new NumArray([1, 2, 3, 4]);
+        int param_1 = obj.SumRange(0, 2);
+        Console.WriteLine(param_1);
+    }
+
+}
